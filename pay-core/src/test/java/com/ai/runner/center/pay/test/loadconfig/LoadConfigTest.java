d0a47f82d16bf3052c7a2be7572616a39457d63f
LiangMeng <liangmeng@asiainfo.com>
2016-05-25 11:02:20 +0800

    初始化slp-pay

---
+++ pay-core/src/test/java/com/ai/runner/center/pay/test/loadconfig/LoadConfigTest.java
@@ -0,0 +1,11 @@
+package com.ai.runner.center.pay.test.loadconfig;
+
+import com.ai.runner.sdk.loader.Load2ConfigCenter;
+
+public class LoadConfigTest {
+
+    public static void main(String[] args) {
+        Load2ConfigCenter.main(new String[]{"F:\\asiainfo\\myJob\\test"});
+    }
+        
+}
